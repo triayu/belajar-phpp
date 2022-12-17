3fa95eeda943cc35f2874d44ce26d581c2c40376
Tri Ayu Novitasari <triayunovitasarivivi>
2022-12-17 18:57:18 +0700

    php-form

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    echo "Hello World";
+
+    $nama="Tri Ayu Novitasari";
+    $umur=21;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaPacar = "Bima";
+    $umurPacar = 22;
+
+    echo "Nama pacar saya <strong>$namaPacar</strong>, pacar saya berusia $umurPacar tahun.<br>";
+
+    $selihUmur = $umur - $umurPacar;
+    echo "Selisih usia saya dan pacar saya adalah $selisihUmur tahun."
+
+?>
